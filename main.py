491047382899aba269c5d0b721d9a6bbefff28a2
1jerry <git-73ry@sneakemail.com>
2019-12-09 16:12:51 -0800

    initial post

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    first_name: str
+    last_name: str
+    zip: int = None
+    phone: int = None
+    email: str = None
+
 
 app = FastAPI()
 
 fake_users_db = [
-    {"user_id": 1, "user_name": "bob"},
-    {"user_id": 2, "user_name": "carl"},
-    {"user_id": 3, "user_name": "abe"},
+    {"user_id": 1, "first_name": "bob"},
+    {"user_id": 2, "first_name": "carl"},
+    {"user_id": 3, "first_name": "abe"},
 ]
 
 
@@ -28,3 +38,11 @@ async def read_user(user_id: int):
 async def read_users(skip: int = 0, limit: int = 10):
     result = fake_users_db[skip:skip + limit]
     return result
+
+
+@app.post('/users/')
+async def create_user(user: User):
+    item_dict = user.dict()
+    item_dict.update({"full_name": user.first_name + " " + user.last_name})
+    fake_users_db.append(item_dict)
+    return item_dict
